81ffbf44d082937bb2725b88f06c29951eedc365
rukaiyafahmida <rukaiyafahmida7@gmail.com>
2023-10-12 20:20:05 +0600

    initial project setup

--- expense_tracker/core/models.py
+++ expense_tracker/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=32, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, blank=True) 
+    type = models.CharField(max_length=8,  blank=False)
+    def __str__(self) -> str:
+        return self.name
+
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.PROTECT, related_name='transactions')
+    descripton = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='transactions')
+    date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.category} {self.currency.code}  {self.amount}  {self.date} "
--- expense_tracker/core/views.py
+++ expense_tracker/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
+from core.models import Currency, Category
+from core.serializers import CurrencySerializer, CategorySerializer
 
-# Create your views here.
+class CurrencyListAPIView(ListAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
+
+
+class CategoryModelViewSet(ModelViewSet):
+    queryset= Category.objects.all()
+    serializer_class = CategorySerializer
